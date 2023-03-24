60a935f0340a5a4f7af6e609dad9371bdd63864d
anna <9408commit@gmail.com>
2023-03-24 09:38:31 +0900

    feat: 로딩 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
+    <LoadingFullsize v-if="isLoading" />
     <div>
       <router-view name="header" class="site-navigation" />
     </div>
     <router-view />
   </div>
 </template>
+<script>
+import { loadingStore } from '@/store/loading'
+import LoadingFullsize from '@/components/loading-fullsize.vue'
 
-<style lang="scss">
-
-</style>
+export default {
+  setup () {
+    const loading = loadingStore()
+    return {
+      loading
+    }
+  },
+  computed: {
+    isLoading () {
+      return this.loading.loading
+    }
+  },
+  components: { LoadingFullsize }
+}
+</script>
